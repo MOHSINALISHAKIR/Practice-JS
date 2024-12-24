9fa3691c39fd0bf9a63de8771277526e2b2c222d
MOHSINALISHAKIR <mohsinalishakir78@gmail.com>
2024-12-24 10:18:02 -0800

    intro to array

---
+++ array.js
@@ -0,0 +1,34 @@
+// const marks_class12=[45,45,24,24];
+//  console.log(marks_class12);
+//  console.log(marks_class12[0]);
+//  console.log(marks_class12[1]);
+//  console.log(marks_class12[2]);
+//  console.log(marks_class12[3]);
+//  console.log(marks_class12[4]);
+//  console.log(marks_class12.length);
+ 
+ 
+
+//arrays methods
+let num=[1,3,5,6,7];
+
+console.log(num);
+console.log("the length of array is",num.length);
+
+let tostring=num.toString();
+console.log(tostring);
+let tojoin=num.join("_");
+console.log(tojoin);
+let topop=num.pop();
+console.log(topop);
+let topush=num.push(7);
+console.log(topush);
+let toshift=num.shift();
+console.log(toshift);
+
+
+
+
+
+
+
